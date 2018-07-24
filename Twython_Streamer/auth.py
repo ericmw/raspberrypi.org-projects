08258818c9e0f5d51178f95ad04be0e593947c5c
ericmw <m.ericmwangi@gmail.com>
2018-07-24 09:14:13 +0300

    Update auth.py

--- Twython_Streamer/auth.py
+++ Twython_Streamer/auth.py
@@ -1,3 +1,4 @@
+#replace these values with those of your twitter API keys from apps.twitter.com
 consumer_key        = 'ABCDEFGHIJKLKMNOPQRSTUVWXYZ'
 consumer_secret     = '1234567890ABCDEFGHIJKLMNOPQRSTUVXYZ'
 access_token        = 'ZYXWVUTSRQPONMLKJIHFEDCBA'
